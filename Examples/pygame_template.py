9addccf46047b401fed52ae6f21398ed03c57bf1
alee <alee@fwparker.org>
2019-04-18 10:26:55 -0500

    ch5

--- Examples/pygame_template.py
+++ Examples/pygame_template.py
@@ -50,3 +50,62 @@ while not done:
 # Close the window and quit.
 pygame.quit()
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Pygame Base Template C Period
+Spring 2019
+by Aaron Lee
+
+
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()  # starts pygame (Vroom!)
+
+# Set the width and height of the screen [width, height]
+screen_width = 700
+screen_height = 500
+size = (screen_width, screen_height)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("Mr. Lee's Game")
+
+done = False  # condition for the game loop
+
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    # --- Game logic should go here
+    # --- Drawing code should go here
+    screen.fill(WHITE)
+
+    pygame.display.flip()  #update the screen
+    clock.tick(60)  #60 frames per second
+
+pygame.quit()  #Close the window and quit.
+
+"""
