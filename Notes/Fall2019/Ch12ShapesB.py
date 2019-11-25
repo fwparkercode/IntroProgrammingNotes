b43c87b0e9750f6691dd6b157aa3773fc94efb56
alee <alee@fwparker.org>
2019-11-25 10:18:29 -0600

    ch13

--- Notes/Fall2019/Ch12ShapesB.py
+++ Notes/Fall2019/Ch12ShapesB.py
@@ -43,10 +43,23 @@ class Circle():
         self.y = 0
         gb = random.randrange(256)  # random green and blue color
         self.color = (255, gb, gb)
+        self.change_x = random.randrange(1, 6)
 
     def draw(self):
         pygame.draw.ellipse(screen, BLACK, [self.x - 2, self.y - 2, self.diameter + 4, self.diameter + 4])
         pygame.draw.ellipse(screen, self.color, [self.x, self.y, self.diameter, self.diameter])
+
+    def update(self):
+        self.x += self.change_x
+        if self.x > SCREEN_WIDTH:
+            self.x = -self.diameter
+
+class Ball(Circle):
+    def __init__(self):
+        super().__init__()
+        rg = random.randrange(256)  # random green and blue color
+        self.color = (rg, rg, 255)
+
 
 
 shape_list = []
@@ -57,6 +70,11 @@ for i in range(100):
     my_circle.y = random.randrange(SCREEN_HEIGHT - my_circle.diameter)
     shape_list.append(my_circle)
 
+    my_ball = Ball()
+    my_ball.x = random.randrange(SCREEN_WIDTH - my_ball.diameter)
+    my_ball.y = random.randrange(SCREEN_HEIGHT - my_ball.diameter)
+    shape_list.append(my_ball)
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop (input from user keyboard, mouse, game controller)
@@ -65,6 +83,8 @@ while not done:
             done = True
 
     # --- Game logic should go here
+    for shape in shape_list:
+        shape.update()
 
     # --- Draw to screen
     screen.fill(WHITE)
