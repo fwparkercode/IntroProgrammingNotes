4aeafc5880486e133cb3e7a478194abda87e2b43
alee <alee@fwparker.org>
2020-01-10 13:13:08 -0600

    ch13

---
+++ Notes/Fall2019/Final Project Example Code C/Animated sprite/animated_sprite.py
@@ -0,0 +1,135 @@
+"""
+Sample Python/Pygame Programs
+Simpson College Computer Science
+http://programarcadegames.com/
+http://simpson.edu/computer-science/
+"""
+
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class Player(pygame.sprite.Sprite):
+    """ The class is the player-controlled sprite. """
+
+    # -- Methods
+    def __init__(self, x, y):
+        """Constructor function"""
+        # Call the parent's constructor
+        super().__init__()
+
+        # Set height, width
+        self.left_image = pygame.image.load("character_femalePerson_walk0_left.png")
+        self.right_image = pygame.image.load("character_femalePerson_walk0.png")
+
+        self.image = self.right_image
+
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        # -- Attributes
+        # Set speed vector
+        self.change_x = 0
+        self.change_y = 0
+
+        self.frame = 0
+        self.image_index = 0
+        self.image_list = [pygame.image.load("character_femalePerson_walk0.png"),
+                           pygame.image.load("character_femalePerson_walk1.png"),
+                           pygame.image.load("character_femalePerson_walk2.png"),
+                           pygame.image.load("character_femalePerson_walk3.png"),
+                           pygame.image.load("character_femalePerson_walk4.png"),
+                           pygame.image.load("character_femalePerson_walk5.png"),
+                           pygame.image.load("character_femalePerson_walk6.png"),
+                           pygame.image.load("character_femalePerson_walk7.png")
+                           ]
+
+    def changespeed(self, x, y):
+        """ Change the speed of the player"""
+        self.change_x += x
+        self.change_y += y
+
+    def update(self):
+        """ Find a new position for the player"""
+        self.frame += 1
+        self.rect.x += self.change_x
+
+        if self.change_x > 0 and self.frame % 3 == 0:
+            self.image_index += 1
+            self.image = self.image_list[self.image_index % len(self.image_list)]
+
+        if self.change_x < 0:
+            self.image = self.left_image
+
+
+
+
+# Call this function so the Pygame library can initialize itself
+pygame.init()
+
+# Create an 800x600 sized screen
+screen = pygame.display.set_mode([800, 600])
+
+# Set the title of the window
+pygame.display.set_caption('Test')
+
+# Create the player object
+player = Player(0, 0)
+player.rect.bottom = screen.get_height()
+all_sprites_list = pygame.sprite.Group()
+all_sprites_list.add(player)
+
+clock = pygame.time.Clock()
+done = False
+
+while not done:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        # Set the speed based on the key pressed
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.changespeed(-3, 0)
+            elif event.key == pygame.K_RIGHT:
+                player.changespeed(3, 0)
+            elif event.key == pygame.K_UP:
+                player.changespeed(0, -3)
+            elif event.key == pygame.K_DOWN:
+                player.changespeed(0, 3)
+
+        # Reset speed when key goes up
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.changespeed(3, 0)
+            elif event.key == pygame.K_RIGHT:
+                player.changespeed(-3, 0)
+            elif event.key == pygame.K_UP:
+                player.changespeed(0, 3)
+            elif event.key == pygame.K_DOWN:
+                player.changespeed(0, -3)
+
+    # --- Game logic
+
+    # This calls update on all the sprites
+    all_sprites_list.update()
+
+    # -- Draw everything
+    # Clear screen
+    screen.fill(WHITE)
+
+    # Draw sprites
+    all_sprites_list.draw(screen)
+
+    # Flip screen
+    pygame.display.flip()
+
+    # Pause
+    clock.tick(60)
+
+pygame.quit()
