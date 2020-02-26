a777f9535d58d12e8cedaee897a12667aa7d31ad
alee <alee@fwparker.org>
2020-02-26 09:07:57 -0600

    ch13

--- Notes/Spring2020/Ch4E.py
+++ Notes/Spring2020/Ch4E.py
@@ -136,3 +136,102 @@ x *= 2  # multiply self by 2
 x /= 3  # divide by 3 (division always gives a float)
 x **= 3  # raise self to power of 3
 print(x)
+
+
+
+# WHILE LOOPS
+
+# 5 to 10 with FOR loop
+for i in range(5, 11):
+    print(i)
+
+
+# 5 to 10 with WHILE loop
+i = 5
+while i < 11:
+    print(i)
+    i += 1
+
+
+# Countdown from 20 to 10
+i = 20
+while i > 9:
+    print(i)
+    i -= 1
+
+# Unknown conditions
+# print all of the squares below a million
+
+n = 1
+
+while n ** 2 < 1000000:
+    print(n, "squared is", n ** 2)
+    n += 1
+
+
+# Infinite loops (problem with while loops)
+
+# condition cannot ever be False
+# while 1 < 2:
+#     print("Yay")
+
+# change in wrong direction
+# x = 10
+# while x > 0:
+#     x += 1
+#     print(x)
+
+# accelerates away from condition
+# x = 2
+# while x > 0:
+#     x *= 2
+#     print(x)
+
+
+# Continuing until user wants to quit
+done = False
+
+# while not done:
+#     answer = input("Do you want to quit?")
+#     if answer.lower() == "y":
+#         done = True
+#
+# print("Thanks for playing!")
+
+
+
+# roll two die 100 times
+# print the sum of the two die
+# how many 7s did you roll
+# how many 12s did you roll
+
+sevens = 0
+twelves = 0
+pairs = 0
+total_rolls = 1000000
+
+for i in range(total_rolls):
+    roll1 = random.randrange(1, 7)
+    roll2 = random.randrange(1, 7)
+    total = roll1 + roll2
+    if total == 7:
+        sevens += 1
+    if total == 12:
+        twelves += 1
+    if roll1 == roll2:
+        pairs += 1
+
+print(sevens / total_rolls * 100, "percent sevens")
+print(twelves / total_rolls * 100, "percent twelves")
+print(pairs / total_rolls * 100, "percent pairs")
+
+
+
+
+
+
+
+
+
+
+
