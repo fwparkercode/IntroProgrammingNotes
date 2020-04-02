4d37917e27d1b9aafc4003dcd813f12a210d954e
alee <alee@fwparker.org>
2020-04-02 10:05:36 -0500

    ch13

---
+++ Notes/Spring2020/Ch8Extras/graphics_and_function.py
@@ -0,0 +1,62 @@
+"""
+Pygame base template
+by Aaron Lee 2020
+"""
+
+import pygame
+
+# Define global varibles
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+MAGENTA = (255, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+WIDTH = 800
+HEIGHT = 600
+done = False
+
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+size = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop (user input)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    # --- Game logic should go here
+
+    # --- Drawing code should go here
+    screen.fill(WHITE)  # paint the blank canvas
+
+    # Head
+    pygame.draw.ellipse(screen, BLACK, [96, 83, 10, 10], 0)
+
+    # Legs
+    pygame.draw.line(screen, BLACK, [100, 100], [105, 110], 2)
+    pygame.draw.line(screen, BLACK, [100, 100], [95, 110], 2)
+
+    # Body
+    pygame.draw.line(screen, RED, [100, 100], [100, 90], 2)
+
+    # Arms
+    pygame.draw.line(screen, RED, [100, 90], [104, 100], 2)
+    pygame.draw.line(screen, RED, [100, 90], [96, 100], 2)
+
+    pygame.display.flip()  # show the updated drawing
+
+    clock.tick(60)  # 60 frames per second
+
+pygame.quit()  # Close the window and quit.
