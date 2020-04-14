b5432db5e9da5064dff2ed28475644e4111e1096
alee <alee@fwparker.org>
2020-04-14 14:56:20 -0500

    ch13

---
+++ Notes/Spring2020/Ch9D.py
@@ -0,0 +1,19 @@
+# Chapter 9 - FUNctions
+
+# functions help organize our code
+# and are repeatable
+
+
+def say_hi():
+    print("Hello")
+
+say_hi()
+
+
+# functions can also take parameters / arguments
+def say_hi2(name):
+    print("Hello", name)
+
+say_hi2("Ben")
+
+
