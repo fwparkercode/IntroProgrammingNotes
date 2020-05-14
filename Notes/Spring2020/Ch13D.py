78b41ceb8020c92aed0d79cd4bca86703bb4f06e
alee <alee@fwparker.org>
2020-05-14 10:33:38 -0500

    ch13

--- Notes/Spring2020/Ch13D.py
+++ Notes/Spring2020/Ch13D.py
@@ -30,12 +30,52 @@ pygame.display.set_caption("My Game")
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    """ The class is the player-controlled sprite. """
+
+    # -- Methods
+    def __init__(self, x, y):
+        """Constructor function"""
+        # Call the parent's constructor
         super().__init__()
-        self.image = pygame.Surface([20, 20])  # create a 20x20 rectangle
+
+        # Set height, width
+        self.image = pygame.Surface([15, 15])
         self.image.fill(BLACK)
+
+        # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        # -- Attributes
+        # Set speed vector
+        self.change_x = 0
+        self.change_y = 0
+
+    def changespeed(self, x, y):
+        """ Change the speed of the player"""
+        self.change_x += x
+        self.change_y += y
+
+    def update(self):
+        """ Find a new position for the player"""
+        self.rect.x += self.change_x
+        if self.rect.left <= 0:
+            self.rect.left = 0
+
+        if self.rect.right >= WIDTH:
+            self.rect.right = WIDTH
+
+
+        self.rect.y += self.change_y
+        if self.rect.top <= 0:
+            self.rect.top = 0
+
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
+
 
 class Block(pygame.sprite.Sprite):
     def __init__(self):
@@ -49,7 +89,7 @@ all_sprites = pygame.sprite.Group()  # bucket for everyone (sprites)
 block_sprites = pygame.sprite.Group()  # bucket for blocks
 
 # create sprites
-player = Player()
+player = Player(0, 0)
 all_sprites.add(player)
 
 for i in range(50):
@@ -59,20 +99,46 @@ for i in range(50):
     all_sprites.add(block)  # makes it update and draw
     block_sprites.add(block)
 
+score = 0
 
 # -------- Main Program Loop -----------
 while not done:
-    # --- Main event loop (user input)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        elif event.type == pygame.MOUSEMOTION:
-            player.rect.right, player.rect.bottom = event.pos
+
+        # Set the speed based on the key pressed
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.changespeed(-3, 0)
+            elif event.key == pygame.K_RIGHT:
+                player.changespeed(3, 0)
+            elif event.key == pygame.K_UP:
+                player.changespeed(0, -3)
+            elif event.key == pygame.K_DOWN:
+                player.changespeed(0, 3)
+
+        # Reset speed when key goes up
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.changespeed(3, 0)
+            elif event.key == pygame.K_RIGHT:
+                player.changespeed(-3, 0)
+            elif event.key == pygame.K_UP:
+                player.changespeed(0, 3)
+            elif event.key == pygame.K_DOWN:
+                player.changespeed(0, -3)
+
 
     # --- Game logic should go here
-    all_sprites.update()
+    all_sprites.update()  # runs the update method for every sprite in the group
 
-    pygame.sprite.spritecollide(player, block_sprites, True)
+    hit_list = pygame.sprite.spritecollide(player, block_sprites, True)
+
+    for block in hit_list:
+        score += 1
+        #block.image.fill(GREEN)
+        print(score)
 
     # --- Drawing code should go here
     screen.fill(WHITE)  # paint the blank canvas
